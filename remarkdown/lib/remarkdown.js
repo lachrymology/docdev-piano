078a694d06f3b1b70ffb8d2a9753a6f075a4bb01
fogus <mefogus@gmail.com>
2012-11-12 11:04:24 -0500

    use the export pattern for node etc

---
+++ remarkdown/lib/remarkdown.js
@@ -0,0 +1,27 @@
+;(function() {
+
+/**
+ * Expose
+ */
+
+remarkdown.parser = function(src, opt) {
+  setOptions(opt);
+  return parse(src);
+};
+
+remarkdown.lexer = function(src, opt) {
+  setOptions(opt);
+  return block.lexer(src);
+};
+
+remarkdown.parse = remarkdown;
+
+if (typeof module !== 'undefined') {
+  module.exports = remarkdown;
+} else {
+  this.remarkdown = remarkdown;
+}
+
+}).call(function() {
+  return this || (typeof window !== 'undefined' ? window : global);
+}());
